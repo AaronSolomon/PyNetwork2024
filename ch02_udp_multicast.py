fc7b6488e29d5870a2b9a21b23b8a5e292ecb6f6
Solomon <solomon@lilina.csie.ncnu.edu.tw>
2024-03-01 16:32:12 +0800

    Add ipv6 & multicast

---
+++ ch02_udp_multicast.py
@@ -0,0 +1,41 @@
+import argparse, random, socket, sys
+
+MAX_BYTES = 65535
+
+def client(hostname, port):
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    # Set the time-to-live for the message (number of hops)
+    ttl = 1
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl)
+    sock.connect((hostname, port))
+    print('Client socket name is {}'.format(sock.getsockname()))
+
+    text = 'This is another message'
+    data = text.encode('ascii')
+    sock.send(data)
+    data = sock.recv(MAX_BYTES)
+    print('The server says {!r}'.format(data.decode('ascii')))
+
+def server(interface, port):
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    sock.bind((interface, port))
+    print('Listening at', sock.getsockname())
+    while True:
+        data, address = sock.recvfrom(MAX_BYTES)
+        text = data.decode('ascii')
+        print('The client at {} says {!r}'.format(address, text))
+        message = 'Your data was {} bytes long'.format(len(data))
+        sock.sendto(message.encode('ascii'), address)
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive UDP,'
+                                     ' pretending packets are often dropped')
+    parser.add_argument('role', choices=choices, help='which role to take')
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='UDP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
